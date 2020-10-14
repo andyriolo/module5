1b359d113ba6e6e983f29bb677ce707fae5b875c
Andrew Riolo <andyriolo17@gmail.com>
2020-10-13 19:24:37 -0700

    added editing features with a date plugin

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,40 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function (){
+ var text = $(this)
+ var textInput = $("<textarea>")
+ $(this).replaceWith(textInput);
+textInput.trigger("focus");
+});
 
+$(".list-group").on("blur", "textarea", function() {
+var text = $(this)
+  .val()
+  .trim();
 
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+  $(this).replaceWith(taskP);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -82,6 +114,59 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+
+//due date was clicked
+
+$(".list-group").on("click", "span", function() { 
+  //get current text
+  var date = $(this)
+  .text()
+  .trim();
+
+  //create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+
+  //swaps out the elements
+  $(this).replaceWith(dateInput);
+
+  //automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+//value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  //get thge parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+
+  //get the tasks position in the list of other li elements
+  var index = $(this)
+  .closesat(".list-group-item")
+  .index();
+
+  //update task in array and re-save to local storage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+  //replace input wit5h span element
+  $(this).replaceWith(taskSpan);
+});
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
